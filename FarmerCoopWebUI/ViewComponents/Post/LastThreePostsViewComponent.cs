535a977c9f5fc42f94141b37f9ae866516a52c97
Adil Usta <adilusta4@gmail.com>
2023-11-11 01:23:40 +0300

    Tema düzenlendi, partiallar ayrıştırıldı.

---
+++ FarmerCoopWebUI/ViewComponents/Post/LastThreePostsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FarmerCoopWebUI.ViewComponents.Post
+{
+    public class LastThreePostsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
